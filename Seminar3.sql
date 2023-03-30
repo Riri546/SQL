ec2072e6342c8c1879d828c7fd8cdd6ea8d3e74c
Лилия <l_loshakova123@mail.ru>
2023-03-30 22:56:06 +0300

    Solution task

--- Seminar3.sql
+++ Seminar3.sql
@@ -1,6 +1,19 @@
 -- 1. Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке.
 SELECT DISTINCT firstname
 FROM users;
+GROUP BY firstname 
+ORDER BY firstname;
 
 -- 2. Выведите количество мужчин старше 35 лет [COUNT].
+SELECT *
+FROM profiles 
+WHERE 
+     TIMESTAMPDIFF(YEAR, birthday, NOW()) > 35
+     AND gender = 'm'
+
 -- 3. Сколько заявок в друзья в каждом статусе? (таблица friend_requests) [GROUP BY].
+SELECT 
+	COUNT(*),
+	status
+FROM friend_requests 
+GROUP BY status
